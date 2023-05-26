0ec071d37e4ada39d9d3651f8f1b973e36d5d79d
Instance01 <97354675+aslezar@users.noreply.github.com>
2023-05-26 11:04:27 +0530

    finalize

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { client_id } from './config';
 
 //CSS
 import './global.css';
-
-//ENV VARS
-const client_id =
-	'451848493009-22s0o4k44u743v4hmikrsbh2f1ij5ln1.apps.googleusercontent.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
