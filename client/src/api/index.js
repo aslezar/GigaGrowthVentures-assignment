ac91e71a74908dea5cd796a0ef68de5099180ebd
Instance01 <97354675+aslezar@users.noreply.github.com>
2023-05-26 09:52:58 +0530

    fixed bug

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,10 +3,13 @@ import axios from 'axios';
 const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 API.interceptors.request.use((req) => {
-	if (localStorage.getItem('user_info') !== null) {
-		req.headers.Authorization = `Bearer ${JSON.parse(
-			localStorage.getItem('user_info').token
-		)}`;
+	if (
+		localStorage.getItem('user_info') &&
+		JSON.parse(localStorage.getItem('user_info')) !== null
+	) {
+		req.headers.Authorization = `Bearer ${
+			JSON.parse(localStorage.getItem('user_info')).token
+		}`;
 	}
 
 	return req;
